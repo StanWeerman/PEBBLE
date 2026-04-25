2627f46d3957cb56cefb7053e02f551177be33a2
rweerman <rensweerman@gmail.com>
2026-04-25 16:32:36 +0200

    Padding splits message into blocks, no actual padding yet. No way to supply parameters to keccak as of now.

--- software/pebble/src/primitives/hashes/keccak/mod.rs
+++ software/pebble/src/primitives/hashes/keccak/mod.rs
@@ -13,12 +13,18 @@ impl<const WIDTH: usize, const RATE: usize> Sponge<WIDTH, RATE> {
         }
     }
     fn padding(msg: Vec<bool>) -> Vec<[bool; RATE]> {
-        todo!()
+        let mut padded_msg: Vec<[bool; RATE]> = vec![];
+        for i in 0..((msg.len() / RATE) - 1) {
+            let mut arr = [false; RATE];
+            arr.copy_from_slice(&msg[(RATE * i)..(RATE * (i + 1))]);
+            padded_msg.append(&mut vec![arr]);
+        }
+        return padded_msg;
     }
     fn run_keccak(&mut self) -> Vec<bool> {
         self.absorb();
         let mut output: Vec<bool> = vec![];
-        for _ in 0..10 {
+        for _ in 0..12 {
             output.extend_from_slice(&self.squeeze());
         }
         return output;
@@ -62,5 +68,20 @@ impl<const WIDTH: usize, const RATE: usize> Sponge<WIDTH, RATE> {
             self.state.permute();
         }
         return Sponge::state_to_block(self.state.get_state());
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn first_test() {
+        println!("Test 1:");
+        let mut sponge: Sponge<50, 10> = Sponge::new(vec![
+            true, true, false, false, false, false, false, false, false, false,
+        ]);
+        let result = sponge.run_keccak();
+        println!("Result is: {:?}", result);
     }
 }
